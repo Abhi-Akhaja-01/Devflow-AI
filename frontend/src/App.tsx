668ae4f0d9ec660260d2d8e6324b71a78ed0085d
Abhi-Akhaja-01 <env>
2026-05-21 15:08:23 +0530

    donedddddddddddddddddddd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
-// const App = () => {
-//   const name: string = "Abhi";
-//   return (
-//     <div className="h-screen flex items-center justify-center">
-//       <h1 className="text-5xl font-bold text-blue-600">{name}</h1>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 const App = () => {
-  let name = "Abhi";
+  const name = "Abhi";
 
-  return <h1>Hello fvfgnfnfkjnfkjgfnkjdfngkjngfkjgfnkj</h1>;
+  return <h1>{name}</h1>;
 };
 
 export default App;
